6d04bf2e646a688c2ff49f864bbc1536910b8735
Angel <angelglezz.7@gmail.com>
2018-09-28 15:05:27 -0500

    Initial commit

---
+++ metas.php
@@ -0,0 +1,18 @@
+<?php
+	$metas = array(
+		"mal" => array(
+			"title" => "Adivina Juan",
+			"description" => "Claramente no sabes ni lo que es un balón… mejor dedícate al box, al póker o qué sé yo",
+			"image" => "images/resultados-1.jpg" 
+		),
+		"maso" => array(
+			"title" => "Adivina Juan",
+			"description" => "Mñeh, pudiste salir mejor. Ni muy muy, ni tan tan. Échale más ganitas. Sí se puede.",
+			"image" => "images/resultados-2.jpg" 
+		),
+		"bien" => array(
+			"title" => "Adivina Juan",
+			"description" => "¡Eres un D10S! ¡El balón y tú son uno mismo!",
+			"image" => "images/resultados-3.jpg" 
+		)
+	);
